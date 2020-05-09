e7594d4ba3b07ccb22a7788d8208fa394182c042
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 17:38:59 -0400

    15

--- exercises/15-Random-numbers/app.js
+++ exercises/15-Random-numbers/app.js
@@ -1,8 +1,6 @@
-/*Exercise closely resembles 11 - consider introducing another concept here? */
-
 function getRandomInt()
 {
-	var randomNumber = Math.random();
+	var randomNumber = Math.floor(Math.random()*9+1);
 	return randomNumber;
 }
 
