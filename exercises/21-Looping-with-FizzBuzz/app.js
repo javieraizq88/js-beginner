a9ef4494a17f72b3130d0e09d804a717ac9f1834
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 18:02:49 -0400

    21

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,5 +1,11 @@
 function fizzBuzz() {  
-	// Your code here
+	//your code here
+	for(let i = 1; i<=100; i++){
+	  
+	  if(i % 3 == 0) console.log("Fizz");
+	  if(i % 5 == 0) console.log("Buzz");
+	  if(i%3 != 0 & i%5 != 0) console.log(i);
+	}
 }
 
 fizzBuzz();
