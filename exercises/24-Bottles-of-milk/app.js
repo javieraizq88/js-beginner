c4168d696e77c24626e66b15593ccbba485a66a4
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 18:53:17 -0400

    24

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,23 @@
-// Your code here:
+
+
+for(let i = 99; i>1; i--){
+    let l = "";
+    l += i+" bottles of milk on the wall, "+i+" bottles of milk."; 
+    l += "Take one down and pass it around, "+(i-1)+" bottles of milk on the wall.";
+    console.log(l);
+    console.log("");
+  }
+    
+    let l = "";
+    l += "1 bottles of milk on the wall, 1 bottles of milk."; 
+    l += "Take one down and pass it around, no more bottles of milk on the wall.";
+        console.log(l);
+      console.log("");
+    l = "";
+    l += "No more bottles of milk on the wall, no more bottles of milk.";
+    l += "Go to the store and buy some more, 99 bottles of milk on the wall.";
+      console.log(l);
+      console.log("");
+  
+  
+  
