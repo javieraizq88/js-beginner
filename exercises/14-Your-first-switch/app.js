961849334f03874ef1ca7a8c02f87d0cca34a183
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 17:37:51 -0400

    14

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,9 +1,21 @@
+
 function getColor(selection)
 {
+  selection = selection.trim().toLowerCase();
+  
 	switch(selection){
 		//Add more options here
+		case "red":
+		  return true
+		  break;
+		case "green":
+		  return true
+		  break;
+		case "blue":
+		  return true
+		  break;
 	    default :
-	    	return false;//returns false because the user picked an unavailable color
+	    	return false;//return false because the user pick a unavailable color
 	    break;               
 	}
 }
@@ -12,3 +24,29 @@ var colorname = window.prompt('What color do you want?');
 var isAvailable = getColor(colorname);
 if(isAvailable) console.log('Good news! That color is available');
 else console.log('We are sorry, that color is not available');
+
+
+ 
+// function getColor(selection)
+// {
+// 	var selection = selection.toLowerCase();
+// 	switch(selection){
+// 		case "red":
+// 			return true;
+// 	    break;               
+// 		case "green":
+// 			return true;
+// 	    break;               
+// 		case "blue":
+// 			return true;
+// 	    break;               
+// 	    default :
+// 	    	return false;//return false because the user pick a unavailable color
+// 	    break;               
+// 	}
+// }
+
+// var colorname = window.prompt('What color do you want?');
+// var isAvailable = getColor(colorname);
+// if(isAvailable) console.log('Good news! That color is available');
+// else console.log('We are sorry, that color is not available');
