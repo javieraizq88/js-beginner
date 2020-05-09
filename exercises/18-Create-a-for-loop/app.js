18ef10317acd27af966452ee76a6a3b64de0e035
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 17:42:23 -0400

    18

--- exercises/18-Create-a-for-loop/app.js
+++ exercises/18-Create-a-for-loop/app.js
@@ -1,7 +1,17 @@
-/* This exercise should be moved before 17-Your first loop? */
 
-// Declare and write your function here:
+//declare and write your function here
+function standardsMaker(){
+    for (i = 0; i < 300; i++){
+      console.log("I will write questions if I'm stuck");
+    }
+  }  
+  
 
+//   function standardsMaker(){
+// 	for (var i = 0; i<300;i++)
+// 	{
+// 		console.log("I will write questions if I'm stuck");
+// 	}
+// }
 
-// Calling the function:
-standardsMaker();
+// standardsMaker();
