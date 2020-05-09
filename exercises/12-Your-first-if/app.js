817e738ee3bf802978c20c556a93b454ef7e1696
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 17:29:40 -0400

    12

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,11 @@
-var total = prompt('How much money do you have in your pocket');
+var total = window.prompt('How much money do you have in your pocket');
 
-// Your code below:
+//your code here
+if (total > 100) 
+    console.log("Give me your money!");
+
+else if(total > 50) 
+    console.log("Buy me some coffee you cheap!");
+
+else if(total <= 50)
+    console.log("You are a poor guy, go away!");
