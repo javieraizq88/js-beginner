08c45c135d6d321a41060cde14b44cbf2950f008
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-09 19:16:53 -0400

    24

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,23 +1,57 @@
+var person = {
+    name: "John",                    //String
+    lastname: "Doe",
+    age: 35,                         //Number
+    gender: "male",
+    lucky_numbers: [ 7, 11, 13, 17], //Array
+    significant_other: person2       //Object, yes the same variable/object defined after
+};
+
+var person2 = {
+    name: "Jane",
+    lastname: "Doe",
+    age: 38,
+    gender: "female",
+    lucky_numbers: [ 2, 4, 6, 8],
+    significant_other: person
+};
 
 
-for(let i = 99; i>1; i--){
-    let l = "";
-    l += i+" bottles of milk on the wall, "+i+" bottles of milk."; 
-    l += "Take one down and pass it around, "+(i-1)+" bottles of milk on the wall.";
-    console.log(l);
-    console.log("");
-  }
+var family = {
+    lastname: "Doe",
+    members: [person, person2]       //Array of objects, don't forget to add Jimmy
+};
+
+//1. Change the fourth lucky number of John Doe to 33
+person.lucky_numbers[3] = 33;
+
+//2. Create a new person and add it to the family object
+var person3 = {
+    name: "Jimmy",
+    lastname: "Doe",
+    age: 13,
+    gender: "male",
+    lucky_numbers: [ 1, 2, 3, 4],
+    significant_other: null
+};
+
+family.members.push(person3);
+
+//3. Now please print ( console.log() ) the SUM of all of the lucky numbers of the Doe family.
+
+function addAllFamilyLuckyNumbers(doeFamily){
+  var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
+  
+  for(var i =0; i < doeFamily.members.length ; i++){
     
-    let l = "";
-    l += "1 bottles of milk on the wall, 1 bottles of milk."; 
-    l += "Take one down and pass it around, no more bottles of milk on the wall.";
-        console.log(l);
-      console.log("");
-    l = "";
-    l += "No more bottles of milk on the wall, no more bottles of milk.";
-    l += "Go to the store and buy some more, 99 bottles of milk on the wall.";
-      console.log(l);
-      console.log("");
+    for(var j=0; j < doeFamily.members[i].lucky_numbers.length ; j++){
+      
+      sumOfAllLuckyNumbers += doeFamily.members[i].lucky_numbers[j];
+    }
+    
+  }
   
-  
-  
+  return sumOfAllLuckyNumbers;
+}
+
+console.log(addAllFamilyLuckyNumbers(family)); 
