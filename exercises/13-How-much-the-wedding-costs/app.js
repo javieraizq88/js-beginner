de539fb30edc03126cd2c7892a323abb9494b377
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-11 13:07:32 -0400

    opcion 2 en 13

--- exercises/13-How-much-the-wedding-costs/app.js
+++ exercises/13-How-much-the-wedding-costs/app.js
@@ -13,14 +13,15 @@ else if (number > 200) price = 20000;
 console.log('Your wedding will cost '+price+' dollars');
 
 
-
+// OPCION 2
+//
 // let userInput = prompt('How many people are comming to your wedding?');
-
+//
 // var price = 0;
 // // your code here
 // if(parseInt(userInput) <= 50) price = 4000;
 // else if(parseInt(userInput) <= 100) price = 10000;
 // else if(parseInt(userInput) <= 200) price = 15000;
 // else if(parseInt(userInput) > 200) price = 20000;
-
+//
 // console.log('Your wedding will cost '+price+' dollars');
